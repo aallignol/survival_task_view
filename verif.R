c7ba349173d7512a36b9d88a160ac6eed6dc39e3
arthur <arthur@biom192>
2012-09-24 12:46:36 +0200

    verif: a test to check availibility of packages in the whole doc
    deleted some old files
    
    Survival: typo

--- verif.R
+++ verif.R
@@ -1,13 +1,22 @@
 require(ctv)
 
-aa <- read.ctv("/home/arthur/surv_task_view/Survival.ctv")
-ctv2html(aa, file = "/home/arthur/surv_task_view/Survival.html")
+myTaskView <- read.ctv("Survival.ctv")
+ctv2html(myTaskView, file = "Survival.html")
 
-bb <- as.vector(available.packages()[,1])
+cran_packages <- as.vector(available.packages()[,1])
 
-## test
-all(aa$packagelist$name %in% bb)
-## should be TRUE
+## compare package list in task view to available packages on CRAN
+all(myTaskView$packagelist$name %in% cran_packages)
+
+
+### same test but in the whole document
+doc <- xmlTreeParse("Survival.ctv",
+                    useInternalNode = TRUE)
+
+x <- getNodeSet(doc, "//*/pkg")
+all_packages <- unique(sapply(x, xmlValue))
+
+all(all_packages %in% cran_packages)
 
 ## setwd("/home/arthur/surv_task_view/")
 ## system("mkdir test_download")
@@ -17,3 +26,31 @@ all(aa$packagelist$name %in% bb)
 ## ## Should be TRUE :-)
 
 ## system("rm -r test_download")
+
+## x <- readLines("Survival.ctv")
+## aa <- grep("/<pkg[^>]*>(.*)<\\/pkg>", x, perl = TRUE)
+
+## doc <- readLines("/misc/devR/surv_task_view/Survival.ctv")
+
+doc <- xmlTreeParse("/misc/devR/surv_task_view/Survival.ctv",
+                    useInternalNode = TRUE)
+
+x <- getNodeSet(doc, "//*/pkg")
+all_packages <- unique(sapply(x, xmlValue))
+
+aa <- x[[1]]
+
+
+newlineSub <- function(x) {
+    for (i in c(":", ",", ";", ")", ".", "?", "!")) x <- gsub(paste("\n[ ]*\\", 
+                                                                    i, sep = ""), i, x)
+    x <- gsub("(\n<a", "(<a", x, fixed = TRUE)
+    return(x)
+}
+
+name <- function(x) xmlValue(x$name)
+topic <- function(x) xmlValue(x$topic)
+maintainer <- function(x) xmlValue(x$maintainer)
+email <- function(x) as.vector(xmlAttrs(x$maintainer)["email"])
+ctvversion <- function(x) xmlValue(x$version)
+info <- function(x) newlineSub(xmlPaste(x$info, indent = "    "))
