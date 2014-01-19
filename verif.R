67848694abc64e594d66ff7f9c8957b47aeb161a
Arthur Allignol <arthur.allignol@uni-ulm.de>
2014-01-19 21:43:04 +0100

    use of a new function of ctv for checking taskview.
    Fix error in the ctv file

--- verif.R
+++ verif.R
@@ -3,6 +3,9 @@ require(ctv)
 myTaskView <- read.ctv("Survival.ctv")
 ctv2html(myTaskView, file = "Survival.html")
 
+## 
+check_ctv_packages("Survival.ctv")
+
 cran_packages <- as.vector(available.packages()[,1])
 
 ## compare package list in task view to available packages on CRAN
